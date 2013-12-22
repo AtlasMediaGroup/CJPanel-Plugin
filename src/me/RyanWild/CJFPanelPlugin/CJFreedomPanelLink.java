2d15c343b00c07ecfb3b5bc66411fea6155eabf8
Ryan <ryan.ictclubsales@gmail.com>
2013-12-22 23:13:43 +0000

    Got the basics set up.

---
+++ src/me/RyanWild/CJFPanelPlugin/CJFreedomPanelLink.java
@@ -0,0 +1,53 @@
+package me.RyanWild.CJFPanelPlugin;
+
+import java.util.logging.Logger;
+import me.RyanWild.CJFPanelPlugin.Commands.*;
+import net.pravian.bukkitlib.command.BukkitCommandHandler;
+import net.pravian.bukkitlib.config.YamlConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CJFreedomPanelLink extends JavaPlugin
+{
+
+    public static final Logger logger = Bukkit.getLogger();
+
+    public static BukkitCommandHandler handler;
+
+    public static YamlConfig config;
+
+    public static CJFreedomPanelLink plugin;
+
+    public static final String MSG_NO_PERMS = ChatColor.RED + "You do not have permission to use this command.";
+
+    @Override
+    public void onEnable()
+    {
+        plugin = this;
+
+        handler = new BukkitCommandHandler(plugin);
+        handler.setCommandLocation(Command_serverkill.class.getPackage());
+        handler.setPermissionMessage(MSG_NO_PERMS);
+
+        config = new YamlConfig(plugin, "config.yml", true);
+        config.load();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        CJFreedomPanelLink.logger.info("LinkMe Plugin Disabled");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        // This will handle ALL commands from now on DONT TOUCH!!!
+        return handler.handleCommand(sender, cmd, commandLabel, args);
+
+    }
+
+}
---
+++ src/me/RyanWild/CJFPanelPlugin/Commands/Command_serverkill.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.RyanWild.CJFPanelPlugin.Commands;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Command_serverkill
+{
+    
+}
