1b0277e68cd5872ef5d50df29e0b4d971b8b3124
Ryan <Ryan.ictclubsales@gmail.com>
2014-11-11 11:57:15 +0000

    Set up some basic permissions for the command types. TFM Is now required.

--- src/com/superiornetworks/cjpanel/commands/Command_server.java
+++ src/com/superiornetworks/cjpanel/commands/Command_server.java
@@ -22,75 +22,112 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import me.StevenLawson.TotalFreedomMod.TFM_AdminList;
 
 @CommandPermissions(source = SourceType.PLAYER)
 public class Command_server extends BukkitCommand
 {
+    
     @Override
     public boolean run(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
         PanelMode mode = PanelMode.DONOTHING;
-
-        if (args.length == 0)
+        
+        if (!TFM_AdminList.isSuperAdmin(commandSender))
         {
-            return false;
-        }
-
-        if (args.length == 1)
+            commandSender.sendMessage(CJPanel.MSG_NO_PERMS);
+        } else
         {
-            if (args[0].equals("reboot"))
+            
+            if (args.length == 0)
             {
-                mode = (PanelMode.REBOOT);
+                return false;
             }
-
-            if (args[0].equals("kill"))
+            
+            if (args.length == 1)
             {
-                mode = (PanelMode.KILL);
+                if (args[0].equals("reboot"))
+                {
+                    if (TFM_AdminList.isSeniorAdmin(commandSender, true))
+                    {
+                    mode = (PanelMode.REBOOT);
+                    }
+                    else
+                    {
+                    commandSender.sendMessage(CJPanel.MSG_NO_PERMS);
+                    }
+                }
+                
+                if (args[0].equals("kill"))
+                {
+                    if (TFM_AdminList.isSeniorAdmin(commandSender, true))
+                    {
+                    mode = (PanelMode.KILL);
+                    }
+                    else
+                    {
+                        commandSender.sendMessage(CJPanel.MSG_NO_PERMS);
+                    }
+                }
+                
+                if (args[0].equals("wipeflatlands"))
+                {
+                    if (TFM_AdminList.isTelnetAdmin(commandSender, true))
+                    {
+                        mode = (PanelMode.WIPEFLAT);
+                    } else
+                    {
+                        commandSender.sendMessage(CJPanel.MSG_NO_PERMS);
+                    }
+                }
+                
+                if (args[0].equals("essentialwipe"))
+                {
+                    if (TFM_AdminList.isSeniorAdmin(commandSender, true))
+                    {
+                    mode = (PanelMode.ESSWIPE);
+                    }
+                    else
+                    {
+                        commandSender.sendMessage(CJPanel.MSG_NO_PERMS);
+                    }
+                }
+                
+                if (args[0].equals("test"))
+                {
+                    mode = (PanelMode.TEST);
+                }
+                
             }
-
-            if (args[0].equals("wipeflatlands"))
+            
+            if (args.length == 2)
             {
-                mode = (PanelMode.WIPEFLAT);
+                return false;
+                
             }
-
-            if (args[0].equals("essentialwipe"))
-            {
-                mode = (PanelMode.ESSWIPE);
-            }
-
-            if (args[0].equals("test"))
-            {
-                mode = (PanelMode.TEST);
-            }
-
+            
+            PanelAccess(commandSender, (Player) commandSender, mode);
+            
+            return true;
         }
-
-        if (args.length == 2)
-        {
-            return false;
-
-        }
-
-     PanelAccess(commandSender, (Player) commandSender, mode);
-
-        return true;
+        return false;
     }
-
+    
     public static void PanelAccess(final CommandSender sender, final Player target, final Command_server.PanelMode mode)
     {
         PanelAccess(sender, target.getName(), target.getAddress().getAddress().getHostAddress().trim(), mode);
     }
-
+    
     public static void PanelAccess(final CommandSender sender, final String targetName, final String targetIP, final PanelMode mode)
     {
         final String PanelURL = CJPanel.config.getString("PANEL_URL");
         final String PanelAPI = CJPanel.config.getString("PANEL_API_KEY");
-
+        
         if (PanelURL == null || PanelAPI == null || PanelURL.isEmpty() || PanelAPI.isEmpty())
         {
             return;
         }
-
+        
         new BukkitRunnable()
         {
             @Override
@@ -102,20 +139,20 @@ public class Command_server extends BukkitCommand
                     {
                         sender.sendMessage(ChatColor.YELLOW + "Connecting you to the panel API - Please Standby...");
                     }
-
+                    
                     URL url = new URLBuilder(PanelURL)
                             .addQueryParameter("apikey", PanelAPI)
                             .addQueryParameter("action", mode.toString())
                             .addQueryParameter("name", targetName)
                             .getURL();
-
+                    
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                     connection.setConnectTimeout(1000 * 5);
                     connection.setReadTimeout(1000 * 5);
                     connection.setUseCaches(false);
-
+                    
                     final int responseCode = connection.getResponseCode();
-
+                    
                     if (sender != null)
                     {
                         new BukkitRunnable()
@@ -126,72 +163,80 @@ public class Command_server extends BukkitCommand
                                 if (responseCode == 200)
                                 {
                                     sender.sendMessage(ChatColor.RED + "The Panel API Access status is UNKNOWN - Contact a CJFreedomMod developer ASAP!");
-                                }
-                                else if (responseCode == 123)
+                                } else
                                 {
-                                    sender.sendMessage(ChatColor.GREEN + "Connection to the Panel API Established. Request to " + mode.toString() + " has been recieved.");
-                                }
-                                else if (responseCode == 201)
-                                {
-                                    sender.sendMessage(ChatColor.GREEN + "A connection to the panel has been established! An action is now required.");
-                                }
-                                else if (responseCode == 121)
-                                {
-                                    sender.sendMessage(ChatColor.RED + "The API has been disabled on the Webserver. Please contact a CJFreedomMod Developer ASAP! ");
-                                }
-                                else if (responseCode == 122)
-                                {
-                                    sender.sendMessage(ChatColor.RED + "The Key located in the servers properties file does not mach the key located on the webserver - Please contact a CJFreedomMod developer ASAP!");
-                                }
-                                else
-                                {
-                                    sender.sendMessage(ChatColor.RED + "There has been a General error conncting to the API - Contact a CJFreedomMod Developer ASAP");
+                                    if (responseCode == 123)
+                                    {
+                                        sender.sendMessage(ChatColor.GREEN + "Connection to the Panel API Established. Request to " + mode.toString() + " has been recieved.");
+                                    } else
+                                    {
+                                        if (responseCode == 201)
+                                        {
+                                            sender.sendMessage(ChatColor.GREEN + "A connection to the panel has been established! An action is now required.");
+                                        } else
+                                        {
+                                            if (responseCode == 121)
+                                            {
+                                                sender.sendMessage(ChatColor.RED + "The API has been disabled on the Webserver. Please contact a CJFreedomMod Developer ASAP! ");
+                                            } else
+                                            {
+                                                if (responseCode == 122)
+                                                {
+                                                    sender.sendMessage(ChatColor.RED + "The Key located in the servers properties file does not mach the key located on the webserver - Please contact a CJFreedomMod developer ASAP!");
+                                                } else
+                                                {
+                                                    sender.sendMessage(ChatColor.RED + "There has been a General error conncting to the API - Contact a CJFreedomMod Developer ASAP");
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }.runTask(TotalFreedomMod.plugin);
                     }
-                }
-                catch (IOException | IllegalArgumentException | IllegalStateException ex)
+                } catch (IOException | IllegalArgumentException | IllegalStateException ex)
                 {
                     TFM_Log.severe(ex);
                 }
             }
         }.runTaskAsynchronously(TotalFreedomMod.plugin);
     }
-
+    
     public static enum PanelMode
     {
+        
         REBOOT("restart"), DONOTHING("donothing"), KILL("kill"), WIPEFLAT("wipeflatlands"), ESSWIPE("clearuserdata"), TEST("tester");
         private final String mode;
-
+        
         private PanelMode(String mode)
         {
             this.mode = mode;
         }
-
+        
         @Override
         public String toString()
         {
             return mode;
         }
     }
-
+    
     private static class URLBuilder
     {
+        
         private final String requestPath;
         private final Map<String, String> queryStringMap = new HashMap<>();
-
+        
         public URLBuilder(String requestPath)
         {
             this.requestPath = requestPath;
         }
-
+        
         public URLBuilder addQueryParameter(String key, String value)
         {
             queryStringMap.put(key, value);
             return this;
         }
-
+        
         public URL getURL() throws MalformedURLException
         {
             List<String> pairs = new ArrayList<>();
@@ -201,7 +246,7 @@ public class Command_server extends BukkitCommand
                 Entry<String, String> pair = it.next();
                 pairs.add(pair.getKey() + "=" + pair.getValue());
             }
-
+            
             return new URL(requestPath + "?" + StringUtils.join(pairs, "&"));
         }
     }
