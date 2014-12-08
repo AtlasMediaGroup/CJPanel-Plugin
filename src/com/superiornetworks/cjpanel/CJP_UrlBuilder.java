98ead9e9c2f350f4f8a31796c402a8bdc5fc0cb0
218303 <218303@F38A-06.newcollege.ac.uk>
2014-12-08 10:12:10 +0000

    Update for 1.8 Craftbukkit.

--- src/com/superiornetworks/cjpanel/CJP_UrlBuilder.java
+++ src/com/superiornetworks/cjpanel/CJP_UrlBuilder.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 
 public class CJP_UrlBuilder
